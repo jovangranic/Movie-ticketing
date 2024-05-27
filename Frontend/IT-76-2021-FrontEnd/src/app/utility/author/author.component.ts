13ee08777b5d57877c7cb371cc2129aa906d0f28
jovangranic <jowan2002@gmail.com>
2024-05-27 10:20:37 +0200

    Doradjene home, author i about metode

--- Frontend/IT-76-2021-FrontEnd/src/app/utility/author/author.component.ts
+++ Frontend/IT-76-2021-FrontEnd/src/app/utility/author/author.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-author',
   templateUrl: './author.component.html',
   styleUrls: ['./author.component.css']
 })
-export class AuthorComponent {
+export class AuthorComponent implements OnInit {
+  constructor() { }
 
+  ngOnInit(): void {
+  }
 }
